53ceb8a4c367df1af312181c49e2e1b85d404149
Victor Zornek <vzornek@example.com>
2025-02-17 10:21:53 -0300

    estrutura inicial do projeto FoodExplorer

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require("express-async-errors");
+
+require("dotenv").config();
+
+const AppError = require("./utils/AppError");
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+app.use(( error, request, response, next ) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
